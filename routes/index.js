f7a02e0b7caba8f97a336a961e3d61ff2cad8e3d
artemkha <khaapt@gmail.com>
2017-08-23 17:45:07 +0300

    initial

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+
+router.get('/register', (req, res, next) => {
+  return res.render('register', { title: 'Sing Up' })
+})
+
+router.post('/register', (req, res, next) => {
+  return res.send('User created!')
+})
+
+// GET /
+router.get('/', function(req, res, next) {
+  return res.render('index', { title: 'Home' });
+});
+
+// GET /about
+router.get('/about', function(req, res, next) {
+  return res.render('about', { title: 'About' });
+});
+
+// GET /contact
+router.get('/contact', function(req, res, next) {
+  return res.render('contact', { title: 'Contact' });
+});
+
+module.exports = router;
