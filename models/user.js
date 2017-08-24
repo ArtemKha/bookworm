06b0422cc30700a60b12ee0dbeb78fd156d8ddb4
artemkha <khaapt@gmail.com>
2017-08-24 12:56:07 +0300

    authentication

---
+++ models/user.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  favoriteBook: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+UserSchema.statics.authenticate = (email, password, cb) => {
+  User.findOne({ email: email})
+      .exec((err, user) => {
+          if (err) return err
+          else if (!user) {
+            const error = new Error('User not found.')
+            error.status = 404
+            return cb(error)
+          }
+          bcrypt.compare(password, user.password, (error, result) => {
+            if (result === true) {
+              return cb(null, user)
+            } else {
+              return cb()
+            }
+          })
+        })
+}
+
+UserSchema.pre('save', (next) => {
+  const user = this
+  bcrypt.hash(user.password, bcrypt.genSaltSync(10), null, (err, hash) => {
+    if (err) return next(err)
+    user.password = hash
+    next()
+  })
+})
+
+const User = mongoose.model('User', UserSchema)
+module.exports = User
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,66 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/user');
 
+router.get('/login', (req, res, next) => {
+  return res.render('login', { title: 'Log In' })
+})
+
+router.post('/login', (req, res, next) => {
+  if (req.body.email && req.body.password) {
+    User.authenticate(req.body.email, req.body.password, function(error, user) {
+      if (error || !user) {
+        const err = new Error('Wrong password or email.')
+        err.status = 401
+        return next(err)
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('./profile')
+      }
+    })
+  } else {
+    const err = new Error('Password and email are required.')
+    err.status = 401
+    return next(err)
+  }
+})
 
 router.get('/register', (req, res, next) => {
   return res.render('register', { title: 'Sing Up' })
 })
 
 router.post('/register', (req, res, next) => {
-  return res.send('User created!')
+  if (req.body.email &&
+    req.body.name &&
+    req.body.favoriteBook &&
+    req.body.password &&
+    req.body.confirmPassword) {
+    if (req.body.password !== req.body.confirmPassword) {
+      const err = new Error('Passwords do not match.')
+      err.status = 400
+      return next(err)
+    }
+    const userData = {
+      email: req.body.email,
+      name: req.body.name,
+      favoriteBook: req.body.favoriteBook,
+      password: req.body.password,
+      confirmPassword: req.body.confirmPassword
+    }
+
+    User.create(userData, (err, user) => {
+      if (err) {
+        return next(err)
+      } else {
+        req.session.userId = user._id;
+        return res.redirect('/profile')
+      }
+    })
+  } else {
+    const err = new Error('All fields required.')
+    err.status = 400
+    return next(err)
+  }
 })
 
 // GET /
